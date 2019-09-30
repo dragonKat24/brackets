9304b90e6f66dcadd2b7699cbe3891d0df5d5021
Catherine Velishek <dragonkat24@gmail.com>
2019-09-30 17:35:48 +0600

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // my solution
+    let result = true,
+        strArr = str.split('');
+
+    function getBracketPair(char) {
+        let pair = false;
+
+        bracketsConfig.map(function(item) {
+            if (char === item[0]) {
+                pair = item[1];
+            }
+        });
+
+        return pair;
+    };
+    
+    function checkStr(open, close) {
+        if (!strArr.length) return;
+
+        if (strArr[close] === getBracketPair(strArr[open])) {
+            strArr.splice(open, 2);
+            open = open - 2 >= 0 ? open - 2 : -1;
+            close = open + 1;
+        } else if (!strArr[close + 1]) {
+            result = false;
+            return;
+        }
+
+        checkStr(open + 1, close + 1);
+    }
+
+    checkStr(0, 1);
+
+    return result;
 }
